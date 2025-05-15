804b82c9ee0ef4c40d677577198af98dc6cfc67a
nick <belyaev.frog01@gmail.com>
2025-05-15 22:06:58 +0300

    policy impl

---
+++ internal/policy/rr/policy.go
@@ -0,0 +1,34 @@
+package rr
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/humanbelnik/load-balancer/internal/server/server"
+)
+
+var (
+	ErrNoServers = errors.New("no servers")
+)
+
+type RoundRobinPolicy struct {
+	m    sync.Mutex
+	next int
+}
+
+func New() *RoundRobinPolicy {
+	return &RoundRobinPolicy{}
+}
+
+func (p *RoundRobinPolicy) Select(servers []server.Server) (server.Server, error) {
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	if len(servers) == 0 {
+		return nil, ErrNoServers
+	}
+
+	s := servers[p.next%len(servers)]
+	p.next++
+	return s, nil
+}
