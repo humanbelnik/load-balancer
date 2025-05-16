c12ab8bce7d83ee896e4b799d7f6aff622920ec5
nick <belyaev.frog01@gmail.com>
2025-05-16 07:26:14 +0300

    rate-limiter abstr

--- internal/balancer/balancer/balancer.go
+++ internal/balancer/balancer/balancer.go
@@ -3,6 +3,7 @@ package balancer
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 
 	"github.com/humanbelnik/load-balancer/internal/balancer/server/server"
@@ -24,12 +25,23 @@ type Policy interface {
 	Select(servers []server.Server) (server.Server, error)
 }
 
+type RateLimiter interface {
+	Allow(ip string) bool
+}
+
 type Balancer struct {
-	pool   Pool
-	policy Policy
+	pool    Pool
+	policy  Policy
+	ratelim RateLimiter
 }
 
 type Option func(*Balancer)
+
+func WithRateLimiter(rl RateLimiter) Option {
+	return func(b *Balancer) {
+		b.ratelim = rl
+	}
+}
 
 /*
 Explicitly defining required parameters.
@@ -48,13 +60,21 @@ func New(pool Pool, policy Policy, opts ...Option) *Balancer {
 }
 
 func (b *Balancer) Serve(w http.ResponseWriter, r *http.Request) {
+	if b.ratelim != nil && !b.HasTicket(w, r) {
+		return
+	}
+
 	log.Println("serve!")
 	aliveServers, err := b.pool.Alive()
 	if err != nil {
 		http.Error(w, "no backends available", http.StatusServiceUnavailable)
 		return
 	}
-	fmt.Println(aliveServers)
+
+	/*
+		Try in loop.
+		If choosen server gave 5xx (his problem) - retry with the next.
+	*/
 	for range aliveServers {
 		srv, err := b.policy.Select(aliveServers)
 		if err != nil {
@@ -71,4 +91,22 @@ func (b *Balancer) Serve(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Error(w, "all backends failed", http.StatusBadGateway)
+}
+
+/*
+Wrap rate limiter's work
+*/
+func (b *Balancer) HasTicket(w http.ResponseWriter, r *http.Request) bool {
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		http.Error(w, "incorrect request", http.StatusBadRequest)
+		return false
+	}
+
+	if !b.ratelim.Allow(ip) {
+		http.Error(w, "rate limit exeed", http.StatusTooManyRequests)
+		return false
+	}
+
+	return true
 }
